384ffa0e6d95f6ca1369f08cd50105f96b5fc2da
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2024-06-30 22:23:00 -0300

    realizando exercício

---
+++ exercicioTS.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplica(n1, n2) {
+    return n1 * n2;
+}
+console.log(multiplica(5, 4));
+function dizOla(nome) {
+    return 'olá ' + nome;
+}
+console.log(dizOla('Rafael'));
---
+++ exercicioTS.ts
@@ -0,0 +1,9 @@
+function multiplica(n1: number, n2: number): number{
+    return n1 * n2
+}
+console.log(multiplica(5,4))
+
+function dizOla(nome: string): string{
+    return 'olá ' + nome
+}
+console.log(dizOla('Rafael'))
